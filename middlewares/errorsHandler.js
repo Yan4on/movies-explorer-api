c9ae8b4d1f5897db313289f460cfefbfa837dc7e
Yanis <yan4on@yandex.ru>
2021-03-15 21:21:35 +0300

    исправить ошибки eslint

--- middlewares/errorsHandler.js
+++ middlewares/errorsHandler.js
@@ -1,6 +1,6 @@
 const { serverError } = require('../utils/messageErr');
 
-const errorsHandler = (err, req, res, next) => {
+const errorsHandler = (err, req, res) => {
   const { statusCode = 500, message } = err;
 
   res.status(statusCode).send({ message: statusCode === 500 ? serverError : message });
--- utils/cors-option.js
+++ utils/cors-option.js
@@ -1,13 +1,13 @@
 const corsOptions = {
   origin: [
-    "http://localhost:3000",
-    "http://localhost:3001",
-    "http://127.0.0.1:3000",
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'http://127.0.0.1:3000',
   ],
-  methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   preflightContinue: false,
   optionsSuccessStatus: 204,
-  allowedHeaders: ["Content-Type", "origin", "x-access-token"],
+  allowedHeaders: ['Content-Type', 'origin', 'x-access-token'],
   credentials: true,
 };
 
